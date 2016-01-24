27df643d38b5b9d63cff2c6ddf07168c0a9f6624
jneferboy <jnefer.boy@gmail.com>
2016-01-25 00:00:07 +0800

    update database.php

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'laravel',
+			'database'  => 'pln-chat',
 			'username'  => 'root',
 			'password'  => 'admin',
 			'charset'   => 'utf8',
